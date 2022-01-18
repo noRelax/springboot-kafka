587089c7ec66deaa378e0a0f4ce92910e08d9c2c
wusong <wusong@8531.com>
2022-01-18 11:34:57 +0800

    fixed codes

--- src/main/java/com/norelax/www/springbootkafka/config/WebConfig.java
+++ src/main/java/com/norelax/www/springbootkafka/config/WebConfig.java
@@ -10,14 +10,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author wusong
  * @date 2022年01月11日 18:30
  */
 @Configuration
-@PropertySource(value = "classpath:application.yml")
 public class WebConfig {
     @Bean
     public Book book() {
